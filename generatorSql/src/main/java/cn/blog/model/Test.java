5f7f4a1c7397df828a8e66eded001177a93731e3
huangxiaohu1 <308869753@qq.com>
2019-04-26 11:44:30 +0800

    '初次提交Mybatis反向工程'

---
+++ generatorSql/src/main/java/cn/blog/model/Test.java
@@ -0,0 +1,91 @@
+package cn.blog.model;
+
+import java.io.Serializable;
+
+public class Test implements Serializable {
+    private Integer id;
+
+    private String userName;
+
+    private Integer cardId;
+
+    private Long moneyAmount;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public Integer getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(Integer cardId) {
+        this.cardId = cardId;
+    }
+
+    public Long getMoneyAmount() {
+        return moneyAmount;
+    }
+
+    public void setMoneyAmount(Long moneyAmount) {
+        this.moneyAmount = moneyAmount;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Test other = (Test) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getUserName() == null ? other.getUserName() == null : this.getUserName().equals(other.getUserName()))
+            && (this.getCardId() == null ? other.getCardId() == null : this.getCardId().equals(other.getCardId()))
+            && (this.getMoneyAmount() == null ? other.getMoneyAmount() == null : this.getMoneyAmount().equals(other.getMoneyAmount()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getUserName() == null) ? 0 : getUserName().hashCode());
+        result = prime * result + ((getCardId() == null) ? 0 : getCardId().hashCode());
+        result = prime * result + ((getMoneyAmount() == null) ? 0 : getMoneyAmount().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", userName=").append(userName);
+        sb.append(", cardId=").append(cardId);
+        sb.append(", moneyAmount=").append(moneyAmount);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
